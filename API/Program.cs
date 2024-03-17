a0e9ccb3f8420349dec4e32498383101bfa44e4f
rieperr <cihato@sabanciuniv.edu>
2024-03-17 17:31:13 +0100

    Frontend init

--- API/Program.cs
+++ API/Program.cs
@@ -11,13 +11,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
